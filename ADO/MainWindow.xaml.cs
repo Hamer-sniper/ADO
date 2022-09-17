8a0eae4eaf4a0bcd49f01e630b73e7aa8e029e98
Andrew Ahverdov <wm-andrew@mail.ru>
2022-09-17 10:08:51 +0300

    Добавьте файлы проекта.

---
+++ ADO/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADO
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WorkWithData workWithData = new WorkWithData();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SqlConnectionStringTextBox.Text = workWithData.sqlConnection.ConnectionString;
+
+            workWithData.sqlConnection.StateChange += new StateChangeEventHandler(OnStateChange);
+        }
+
+        public void OnStateChange(object sender, StateChangeEventArgs args)
+        {
+            SqlConnectionStateTextBox.Text = args.CurrentState.ToString();
+            if (args.CurrentState == System.Data.ConnectionState.Closed)
+                SqlInfoShowButton.Content = "Открыть соединение с SQL";
+            else
+                SqlInfoShowButton.Content = "Закрыть соединение с SQL";
+        }
+
+        private void SqlInfoShowButton_Click(object sender, RoutedEventArgs e)
+        {            
+            workWithData.SqlConnectionStateChanger();
+        }
+    }
+}
---
+++ ADO/WorkWithData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace ADO
+{
+    /// <summary>
+    /// Работа с базами данных
+    /// </summary>
+    public class WorkWithData
+    {
+        #region Поля
+        /// <summary>
+        /// Подключение к SQL
+        /// </summary>
+        public SqlConnection sqlConnection { get; set; }
+        /// <summary>
+        /// Строка подключения к Acess
+        /// </summary>
+        //static string accessConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Programming\DzSkillbox\ADO\ADO\Data\MSAccessLocalDB.accdb;Persist Security Info=True";
+        #endregion
+
+        public WorkWithData()
+        {
+            SqlConnectionStringBuilder sqlCon = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB",
+                InitialCatalog = @"MSSQLLocalDemo",
+                IntegratedSecurity = true,
+                UserID = "sa",
+                Password = "123",
+                Pooling = false
+            };
+            sqlConnection = new SqlConnection(sqlCon.ConnectionString);
+        }
+
+        public void SqlConnectionStateChanger()
+        {
+            if (sqlConnection.State == ConnectionState.Closed)
+                sqlConnection.Open();
+            else
+                sqlConnection.Close();
+        }
+    }
+}
